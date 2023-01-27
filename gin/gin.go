6b6d1627df7eecd72911b72b885c9014964e5b6a
Philip Laine <philip.laine@gmail.com>
2023-01-27 16:39:03 +0100

    Run go fmt

--- gin/gin.go
+++ gin/gin.go
@@ -6,18 +6,18 @@ import (
 	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
 	metricsmiddleware "github.com/slok/go-http-metrics/middleware"
 	ginmetricsmiddleware "github.com/slok/go-http-metrics/middleware/gin"
-	
-  "github.com/xenitab/pkg/gin/middleware"
+
+	"github.com/xenitab/pkg/gin/middleware"
 )
 
 func Default(logger logr.Logger, handlerID string) *gogin.Engine {
-  gogin.SetMode(gogin.ReleaseMode)
-  mdlw := metricsmiddleware.New(metricsmiddleware.Config{
+	gogin.SetMode(gogin.ReleaseMode)
+	mdlw := metricsmiddleware.New(metricsmiddleware.Config{
 		Recorder: metrics.NewRecorder(metrics.Config{}),
 	})
 	engine := gogin.New()
-  engine.Use(middleware.Logger(logger, false))
-  engine.Use(ginmetricsmiddleware.Handler(handlerID, mdlw))
-  engine.Use(gogin.Recovery())
+	engine.Use(middleware.Logger(logger, false))
+	engine.Use(ginmetricsmiddleware.Handler(handlerID, mdlw))
+	engine.Use(gogin.Recovery())
 	return engine
 }
