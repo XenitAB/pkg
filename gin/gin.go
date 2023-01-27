42047c9807742989ec52bd6043c2ef5f95cc10f5
Philip Laine <philip.laine@gmail.com>
2023-01-27 17:00:58 +0100

    Refactor gin engine configuration

--- gin/gin.go
+++ gin/gin.go
@@ -1,23 +1,58 @@
 package gin
 
 import (
+	"regexp"
+
 	gogin "github.com/gin-gonic/gin"
 	"github.com/go-logr/logr"
 	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
 	metricsmiddleware "github.com/slok/go-http-metrics/middleware"
 	ginmetricsmiddleware "github.com/slok/go-http-metrics/middleware/gin"
-
-	"github.com/xenitab/pkg/gin/middleware"
 )
 
-func Default(logger logr.Logger, handlerID string) *gogin.Engine {
+type Config struct {
+	LogConfig     LogConfig
+	MetricsConfig MetricsConfig
+}
+
+type LogConfig struct {
+	// Logger instance to output request logs.
+	Logger logr.Logger
+	// Regex used to filter out request logs.
+	PathFilter *regexp.Regexp
+	// Should request logs should include latency.
+	IncludeLatency bool
+	// Should request logs should include client IP.
+	IncludeClientIP bool
+}
+
+type MetricsConfig struct {
+	// Handler ID to use when using dynamic path parameters.
+	HandlerID string
+}
+
+func DefaultConfig() Config {
+	return Config{
+		LogConfig: LogConfig{
+			Logger:          logr.Discard(),
+			PathFilter:      nil,
+			IncludeLatency:  true,
+			IncludeClientIP: false,
+		},
+		MetricsConfig: MetricsConfig{
+			HandlerID: "",
+		},
+	}
+}
+
+func NewEngine(cfg Config) *gogin.Engine {
 	gogin.SetMode(gogin.ReleaseMode)
 	mdlw := metricsmiddleware.New(metricsmiddleware.Config{
 		Recorder: metrics.NewRecorder(metrics.Config{}),
 	})
 	engine := gogin.New()
-	engine.Use(middleware.Logger(logger, false))
-	engine.Use(ginmetricsmiddleware.Handler(handlerID, mdlw))
+	engine.Use(Logger(cfg.LogConfig))
+	engine.Use(ginmetricsmiddleware.Handler(cfg.MetricsConfig.HandlerID, mdlw))
 	engine.Use(gogin.Recovery())
 	return engine
 }
---
+++ gin/logger.go
@@ -0,0 +1,51 @@
+package gin
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/multierr"
+)
+
+func Logger(cfg LogConfig) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Do not log if path matches filter.
+		if cfg.PathFilter.MatchString(c.Request.URL.Path) {
+			c.Next()
+			return
+		}
+
+		// Start timer
+		start := time.Now()
+
+		// Process request
+		c.Next()
+
+		// Stop timer
+		latency := time.Now().Sub(start)
+
+		// Log request
+		path := c.Request.URL.Path
+		statusCode := c.Writer.Status()
+		kvs := []interface{}{"path", path, "status", statusCode, "method", c.Request.Method}
+		if cfg.IncludeLatency {
+			kvs = append(kvs, "latency", latency)
+		}
+		if cfg.IncludeClientIP {
+			kvs = append(kvs, "ip", c.ClientIP())
+		}
+
+		// Info log if 2xx response
+		if statusCode >= 200 && statusCode < 300 {
+			cfg.Logger.Info("", kvs...)
+			return
+		}
+
+		// Error log if any other status and include error message
+		var err error
+		for _, e := range c.Errors {
+			err = multierr.Append(err, e.Err)
+		}
+		cfg.Logger.Error(err, "", kvs...)
+	}
+}
