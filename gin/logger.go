54cdee7ee435e7a8a42f252a6bd27ec040cba92f
Philip Laine <philip.laine@gmail.com>
2023-02-06 09:38:27 +0100

    Log request context keys

--- gin/logger.go
+++ gin/logger.go
@@ -34,6 +34,13 @@ func Logger(cfg LogConfig) gin.HandlerFunc {
 		if cfg.IncludeClientIP {
 			kvs = append(kvs, "ip", c.ClientIP())
 		}
+		for _, key := range cfg.IncludeKeys {
+			v, ok := c.Keys[key]
+			if !ok {
+				continue
+			}
+			kvs = append(kvs, key, v)
+		}
 
 		// Info log if 2xx response
 		if statusCode >= 200 && statusCode < 300 {
